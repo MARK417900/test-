f4ee4156486450247256c25187ed5b61a33db3f8
MARK417900 <anujmaurya8057@gmail.com>
2026-02-19 23:50:21 +0530

    Refactor service worker caching strategy

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,23 @@
-const CACHE_NAME = "marks-zone-v1";
-
+const CACHE_NAME = "markszone-v1";
 const urlsToCache = [
-  "index.html",
-  "logo.jpg",
-  "Hotya.jpg",
-  "xo.jpg",
-  "GOSH.jpg",
-  "coming soon.jpg"
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js"
 ];
 
-// Install Service Worker
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-// Fetch from Cache
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
